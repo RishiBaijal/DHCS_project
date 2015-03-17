a0873847cbce020b53436170b9d74a482731ad13
Simran Bakshi <simranjit12105@iiitd.ac.in>
2015-03-18 01:18:16 +0530

    Test commit

--- Android/Focus/app/src/main/java/in/ac/iiitd/dhcs/focus/MainTabs/StatsFragment.java
+++ Android/Focus/app/src/main/java/in/ac/iiitd/dhcs/focus/MainTabs/StatsFragment.java
@@ -16,6 +16,7 @@ import in.ac.iiitd.dhcs.focus.R;
  * create an instance of this fragment.
  */
 public class StatsFragment extends Fragment {
+    //Simmooo's fragment
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -24,6 +25,7 @@ public class StatsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
 
 
     /**
