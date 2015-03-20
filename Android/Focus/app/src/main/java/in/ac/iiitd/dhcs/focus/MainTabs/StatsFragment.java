c7e40be6215619e281ebc47f275825f68b82a639
Simran Bakshi <simranjit12105@iiitd.ac.in>
2015-03-20 12:39:13 +0530

    Made changes to graphs

--- Android/Focus/app/src/main/java/in/ac/iiitd/dhcs/focus/MainTabs/StatsFragment.java
+++ Android/Focus/app/src/main/java/in/ac/iiitd/dhcs/focus/MainTabs/StatsFragment.java
@@ -1,6 +1,7 @@
 package in.ac.iiitd.dhcs.focus.MainTabs;
 
 
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
 
 import org.achartengine.chart.PointStyle;
+import org.achartengine.model.TimeSeries;
 import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
@@ -29,6 +31,8 @@ import android.graphics.Color;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Random;
 
@@ -92,154 +96,85 @@ public class StatsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_stats, container, false);
-        openPieChart();
-        openScatterChart();
+        //openPieChart();
+        //openScatterChart();
+        openTimeChart();
         return mView;
     }
 
-    private void openPieChart() {
-        String[] appNamesArray = new String[] {
-                "Whatsapp","Facebook","Abc","PQR","XYZ","TWITTER"
-        };
+    private void openTimeChart(){
 
-        // Pie Chart Section Value
-        double[] usageArray = { 3.9, 12.9, 55.8, 1.9, 23.7, 1.8 } ;
-
-        // Color of each Pie Chart Sections
-        int[] colors = { Color.BLUE, Color.MAGENTA, Color.GREEN, Color.CYAN, Color.RED,
-                Color.YELLOW };
-
-        // Instantiating CategorySeries to plot Pie Chart
-        CategorySeries distributionSeries = new CategorySeries("APP USAGE FOR TODAY! ");
-        for(int i=0 ;i < usageArray.length;i++){
-            // Adding a slice with its values and name to the Pie Chart
-            distributionSeries.add(appNamesArray[i], usageArray[i]);
+        int count = 5;
+        Date[] dt = new Date[5];
+        for(int i=0;i<count;i++){
+            GregorianCalendar gc = new GregorianCalendar(2015, 2, i+1);
+            dt[i] = gc.getTime();
         }
 
-        // Instantiating a renderer for the Pie Chart
-        DefaultRenderer defaultRenderer  = new DefaultRenderer();
-        for(int i = 0 ;i<usageArray.length;i++) {
-            SimpleSeriesRenderer seriesRenderer = new SimpleSeriesRenderer();
-            seriesRenderer.setColor(colors[i]);
-            seriesRenderer.setDisplayBoundingPoints(true);
-            // Adding a renderer for a slice
-            defaultRenderer.addSeriesRenderer(seriesRenderer);
+        int[] prodVals = { 2000,2500,2700,2100,2800};
+
+        TimeSeries prodSeries = new TimeSeries("PRODUCTIVITY");
+
+        for(int i=0;i<dt.length;i++){
+            prodSeries.add(dt[i], prodVals[i]);
+
         }
-        defaultRenderer.setChartTitle("Today's usage Statistics ");
-        defaultRenderer.setChartTitleTextSize(60);
-        defaultRenderer.setShowLabels(true);
-        defaultRenderer.setLabelsTextSize(40);
-        defaultRenderer.setLabelsColor(Color.BLACK);
-        defaultRenderer.setZoomButtonsVisible(true);
+
+        // Creating a dataset to hold each series
+        final XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
 
 
-        GraphicalView chartView;
-		/*if (chartView != null) {
-			chartView.repaint();
-		}
-		else { */
-        chartView = ChartFactory.getPieChartView(getActivity().getApplicationContext(),distributionSeries,defaultRenderer);
-        LinearLayout layout = (LinearLayout) mView.findViewById(R.id.dashboard_chart_layout);
-        //layout.removeAllViews();
-        layout.addView(chartView);
+        dataset.addSeries(prodSeries);
 
-    }
 
-    private void openScatterChart(){
-        String[] titles = new String[] { "XYZ", "ABC", "FAECBOOK", "GMAIL", "WHATSAPP" };
-        List<double[]> x = new ArrayList<>();
-        List<double[]> values = new ArrayList<double[]>();
-        int count = 20;
-        int length = titles.length;
-        Random r = new Random();
-        for (int i = 0; i < length; i++) {
-            double[] xValues = new double[count];
-            double[] yValues = new double[count];
-            for (int k = 0; k < count; k++) {
-                xValues[k] = k + r.nextInt() % 10;
-                yValues[k] = k * 2 + r.nextInt() % 10;
-            }
-            x.add(xValues);
-            values.add(yValues);
-        }
-        int[] colors = new int[] { Color.BLUE, Color.CYAN, Color.MAGENTA, Color.LTGRAY, Color.GREEN };
-        PointStyle[] styles = new PointStyle[] { PointStyle.X, PointStyle.DIAMOND, PointStyle.TRIANGLE,
-                PointStyle.SQUARE, PointStyle.CIRCLE };
-        XYMultipleSeriesRenderer renderer = buildRenderer(colors, styles);
-        setChartSettings(renderer, "TODAY'S USAGE", "X", "Y", -10, 30, -10, 51, Color.GRAY,
-                Color.LTGRAY);
-        renderer.setXLabels(10);
-        renderer.setYLabels(10);
-        length = renderer.getSeriesRendererCount();
-        for (int i = 0; i < length; i++) {
-            ((XYSeriesRenderer) renderer.getSeriesRendererAt(i)).setFillPoints(true);
-        }
-        // return ChartFactory.getScatterChartIntent(getActivity().getApplicationContext(), buildDataset(titles, x, values), renderer);
-        // Getting a reference to LinearLayout of the MainActivity Layout
+        // Creating XYSeriesRenderer to customize visitsSeries
+        XYSeriesRenderer prodRenderer = new XYSeriesRenderer();
+        prodRenderer.setColor(Color.MAGENTA);
+        prodRenderer.setPointStyle(PointStyle.CIRCLE);
+        prodRenderer.setPointStrokeWidth(5);
+        prodRenderer.setFillPoints(true);
+        prodRenderer.setLineWidth(2f);
+        prodRenderer.setDisplayChartValues(true);
+        prodRenderer.setChartValuesTextSize(25);
+        prodRenderer.setColor(getResources().getColor(R.color.accent));
+        //prodRenderer.setShowLegendItem(false);
+
+
+        // Creating a XYMultipleSeriesRenderer to customize the whole chart
+        XYMultipleSeriesRenderer multiRenderer = new XYMultipleSeriesRenderer();
+
+       // multiRenderer.setChartTitle("PRODUCTIVITY VS TIME Chart");
+        multiRenderer.setXTitle("Days");
+        multiRenderer.setYTitle("Time");
+        multiRenderer.setXAxisColor(Color.BLACK);
+        multiRenderer.setYAxisColor(Color.BLACK);
+        multiRenderer.setAxisTitleTextSize(40);
+        multiRenderer.setXLabels(20);
+        multiRenderer.setYLabels(20);
+        multiRenderer.setYAxisAlign(Paint.Align.LEFT,0);
+        multiRenderer.setZoomButtonsVisible(true);
+        multiRenderer.setMarginsColor(Color.argb(0xff, 0xf0, 0xf0, 0xf0));
+
+
+        multiRenderer.addSeriesRenderer(prodRenderer);
+
         LinearLayout chartContainer = (LinearLayout) mView.findViewById(R.id.dashboard_time_layout2);
 
         // Creating a Time Chart
-        GraphicalView ChartView = (GraphicalView) ChartFactory.getScatterChartView(getActivity().getApplicationContext(), buildDataset(titles, x, values), renderer);
-
+        GraphicalView ChartView;// = (GraphicalView) ChartFactory.getView(getActivity().getApplicationContext(), dataset, multiRenderer);
+        ChartView = (GraphicalView) ChartFactory.getTimeChartView(getActivity().getApplicationContext(), dataset, multiRenderer,"dd-MMM-yyyy");
         chartContainer.addView(ChartView);
 
 
+
+
+
     }
 
-    protected XYMultipleSeriesRenderer buildRenderer(int[] colors, PointStyle[] styles) {
-        XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
-        setRenderer(renderer, colors, styles);
-        return renderer;
-    }
 
-    protected void setRenderer(XYMultipleSeriesRenderer renderer, int[] colors, PointStyle[] styles) {
-        renderer.setAxisTitleTextSize(16);
-        renderer.setChartTitleTextSize(20);
-        renderer.setLabelsTextSize(15);
-        renderer.setLegendTextSize(15);
-        renderer.setPointSize(5f);
-        renderer.setMargins(new int[] { 20, 30, 15, 20 });
-        int length = colors.length;
-        for (int i = 0; i < length; i++) {
-            XYSeriesRenderer r = new XYSeriesRenderer();
-            r.setColor(colors[i]);
-            r.setPointStyle(styles[i]);
-            renderer.addSeriesRenderer(r);
-        }
-    }
-    protected void setChartSettings(XYMultipleSeriesRenderer renderer, String title, String xTitle,
-                                    String yTitle, double xMin, double xMax, double yMin, double yMax, int axesColor,
-                                    int labelsColor) {
-        renderer.setChartTitle(title);
-        renderer.setXTitle(xTitle);
-        renderer.setYTitle(yTitle);
-        renderer.setXAxisMin(xMin);
-        renderer.setXAxisMax(xMax);
-        renderer.setYAxisMin(yMin);
-        renderer.setYAxisMax(yMax);
-        renderer.setAxesColor(axesColor);
-        renderer.setLabelsColor(labelsColor);
-    }
-    protected XYMultipleSeriesDataset buildDataset(String[] titles, List<double[]> xValues,
-                                                   List<double[]> yValues) {
-        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
-        addXYSeries(dataset, titles, xValues, yValues, 0);
-        return dataset;
-    }
-    public void addXYSeries(XYMultipleSeriesDataset dataset, String[] titles, List<double[]> xValues,
-                            List<double[]> yValues, int scale) {
-        int length = titles.length;
-        for (int i = 0; i < length; i++) {
-            XYSeries series = new XYSeries(titles[i], scale);
-            double[] xV = xValues.get(i);
-            double[] yV = yValues.get(i);
-            int seriesLength = xV.length;
-            for (int k = 0; k < seriesLength; k++) {
-                series.add(xV[k], yV[k]);
-            }
-            dataset.addSeries(series);
-        }
-    }
+
+
+
 
 
 
